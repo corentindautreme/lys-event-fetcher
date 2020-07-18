2cd28c18a113b017478289ded4087aea5428e474
Corentin Dautreme <co@fedora.home>
2020-07-18 15:28:22 +0200

    Fixing tests

--- test/test_extraction_utils.py
+++ test/test_extraction_utils.py
@@ -4,7 +4,7 @@ from utils.extraction_utils import check_for_repetition_expression
 
 class ExtractionUtilsTest(unittest.TestCase):
     def test_if_start_every_until_expression_then_should_extract_all_events(self):
-        sentence = "Melodi Grand Prix will start on January 9 and run every Saturday until February 13."
+        sentence = "Melodi Grand Prix will start on January 9 and run every Saturday until February 13"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 6)
         self.assertTrue(events[0][1].strftime("%Y-%m-%d")[5:] == "01-09")
@@ -18,7 +18,7 @@ class ExtractionUtilsTest(unittest.TestCase):
         self.assertTrue(events[0][0] == "start on January 9 and run every Saturday until February 13")
 
     def test_if_another_start_every_end_expression_then_should_extract_all_events(self):
-        sentence = "Melodifestivalen will start on February 6 and run every Saturday until March 13."
+        sentence = "Melodifestivalen will start on February 6 and run every Saturday until March 13"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 6)
         self.assertTrue(events[0][1].strftime("%Y-%m-%d")[5:] == "02-06")
@@ -32,7 +32,7 @@ class ExtractionUtilsTest(unittest.TestCase):
         self.assertTrue(events[0][0] == "start on February 6 and run every Saturday until March 13")
 
     def test_if_every_from_to_expression_then_should_extract_all_events(self):
-        sentence = "Sanremo will run every night from February 4 to February 8."
+        sentence = "Sanremo will run every night from February 4 to February 8"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 5)
         self.assertTrue(events[0][1].strftime("%Y-%m-%d")[5:] == '02-04')
@@ -45,7 +45,7 @@ class ExtractionUtilsTest(unittest.TestCase):
         self.assertTrue(events[0][0] == "every night from February 4 to February 8")
 
     def test_if_another_every_from_to_expression_then_should_extract_all_events(self):
-        sentence = "Melodi Grand Prix will take place every Saturday from January 9 to February 13."
+        sentence = "Melodi Grand Prix will take place every Saturday from January 9 to February 13"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 6)
         self.assertTrue(events[0][1].strftime("%Y-%m-%d")[5:] == "01-09")
@@ -59,7 +59,7 @@ class ExtractionUtilsTest(unittest.TestCase):
         self.assertTrue(events[0][0] == "every Saturday from January 9 to February 13")
 
     def test_if_from_to_every_expression_then_should_extract_all_events(self):
-        sentence = "Melodi Grand Prix will run from January 9 to February 13 every Saturday."
+        sentence = "Melodi Grand Prix will run from January 9 to February 13 every Saturday"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 6)
         self.assertTrue(events[0][1].strftime("%Y-%m-%d")[5:] == "01-09")
@@ -73,7 +73,7 @@ class ExtractionUtilsTest(unittest.TestCase):
         self.assertTrue(events[0][0] == "from January 9 to February 13 every Saturday")
 
     def test_if_another_from_to_every_expression_then_should_extract_all_events(self):
-        sentence = "Melodifestivalen will run from February 6 to March 13 every Saturday."
+        sentence = "Melodifestivalen will run from February 6 to March 13 every Saturday"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 6)
         self.assertTrue(events[0][1].strftime("%Y-%m-%d")[5:] == "02-06")
@@ -87,7 +87,7 @@ class ExtractionUtilsTest(unittest.TestCase):
         self.assertTrue(events[0][0] == "from February 6 to March 13 every Saturday")
 
     def test_if_from_to_expression_without_frequency_token_then_should_default_to_daily_and_extract_all_events(self):
-        sentence = "Sanremo 2021 will take place between February 4 and February 8."
+        sentence = "Sanremo 2021 will take place between February 4 and February 8"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 5)
         self.assertTrue(events[0][1].strftime("%Y-%m-%d")[5:] == '02-04')
@@ -100,6 +100,6 @@ class ExtractionUtilsTest(unittest.TestCase):
         self.assertTrue(events[0][0] == "between February 4 and February 8")
 
     def test_if_from_to_expression_without_frequency_token_then_should_default_to_daily_but_find_that_the_interval_is_too_large_and_extract_no_event(self):
-        sentence = "Sanremo 2021 will take place between February 4 and March 28."
+        sentence = "Sanremo 2021 will take place between February 4 and March 28"
         events = check_for_repetition_expression(sentence)
         self.assertTrue(len(events) == 0)
